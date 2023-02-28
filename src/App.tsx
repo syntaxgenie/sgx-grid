9bc7fd4ee3bf95b8fb2abb72e32ffe66f45ef1c2
Prabhath Madhushan <prabathmadushan5@gmail.com>
2023-02-28 16:06:05 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Demo from './demo';
+
+function App() {
+  return (
+    <div className="App">
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
